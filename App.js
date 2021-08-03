e723451e71b6e48db15a72843b16865a93a5dd7d
Norudah <pierucci.romain@gmail.com>
2021-08-03 15:03:36 +0200

    First styles with StyleSheet

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.layout}>
+      <View style={styles.topBar}>
+        <TextInput style={styles.input} placeholder="Entrer un goal ici..." />
+        <Button title="Ajouter" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  layout: {
+    padding: 50,
+  },
+
+  topBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  input: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginRight: 15,
   },
 });
